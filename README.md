d5e60f173bd89be58aaea0947ff26573ee333de6
cyk <935765353@qq.com>
2023-12-12 00:46:33 +0800

    express

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ npm install -g cnpm --registry=https://registry.npm.taobao.org
 cnpm install -g express-generator
 3、创建express工程，cd到想要创建工程的目录下然后执行express
 express
+4、把打包好的web工程放置到public目录下
+5、执行npm start
+6、浏览器输入http://127.0.0.1:3000，就可以看到public里的项目页面了
 
--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-console.log(path.join(__dirname, 'public'));
+// console.log(path.join(__dirname, 'public'));
 module.exports = app;
